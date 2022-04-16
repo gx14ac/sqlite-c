5f8eb4a93af7fcbd3861866f07ef165624668ad7
Shintarou Okumura <flekystyley@gmail.com>
2022-04-16 15:16:34 +0900

    update main

--- main.c
+++ main.c
@@ -3,6 +3,39 @@
 #include <string.h>
 #include <stdbool.h>
 
+/* define the column size */
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 25
+
+/* define the table layout */
+// safely measure the size of a structure's attributes
+#define size_of_attrubutes(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+/*
+ 	| column     | size | offset |
+	------------------------------
+	| id	     |   4  |    0   |
+	------------------------------
+	| username	 |  32  |    4   |
+	------------------------------
+	| email	     | 255  |   36   |
+	------------------------------
+	| total	     | 291  |        |
+*/
+const uint32_t ID_SIZE = size_of_attrubutes(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attrubutes(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attrubutes(Row, email);
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+
+typedef struct {
+	uint32_t id;
+	char username[COLUMN_USERNAME_SIZE];
+	char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 typedef struct {
 	char* buffer;
 	size_t buffer_length;
@@ -16,7 +49,8 @@ typedef enum {
 
 typedef enum {
 	PREPARE_SUCCESS,
-	PREPARE_UNRECOGNIZED_STATEMENT
+	PREPARE_UNRECOGNIZED_STATEMENT,
+	PREPARE_SYNTAX_ERROR
 } PrepareResult;
 
 typedef enum {
@@ -26,6 +60,7 @@ typedef enum {
 
 typedef struct {
 	StatementType type;
+	Row row_to_insert;
 } Statement;
 
 static InputBuffer* new_input_buffer();
@@ -35,6 +70,8 @@ static void print_prompt();
 static MetaCommandResult do_meta_command(InputBuffer* input_buffer);
 static PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement);
 static void execute_statement(Statement* statement);
+static void serialize_row(Row* source, void* destination);
+static void deserialize_row(Row* source, void* destination);
 
 static InputBuffer* new_input_buffer() {
 	InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -53,6 +90,7 @@ static void read_input(InputBuffer* input_buffer) {
 		exit(EXIT_FAILURE);
 	}
 
+	// ignore traling newline
   	input_buffer->input_length = bytes_read - 1;
   	input_buffer->buffer[bytes_read - 1] = 0;
 }
@@ -76,6 +114,12 @@ static MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 static PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
 	if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
 		statement->type = STATEMENT_INSERT;
+		int args_assigned = sscanf(
+			input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+			statement->row_to_insert.username, statement->row_to_insert.email);
+		if (args_assigned < 3) {
+			return PREPARE_SYNTAX_ERROR;
+		}
 		return PREPARE_SUCCESS;
 	}
 	if (strcmp(input_buffer->buffer, "select") == 0) {
