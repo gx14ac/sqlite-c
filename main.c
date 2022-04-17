d9ce5eb332b0a691a81f97bd7eaeed03bb77e8b5
Shintarou Okumura <flekystyley@gmail.com>
2022-04-17 14:06:43 +0900

    added cache function

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 // define the column size
 #define COLUMN_USERNAME_SIZE 32
@@ -42,13 +45,18 @@ const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 /* define the table layout */
 #define TABLE_MAX_PAGES 100
 const uint32_t PAGE_SIZE = 4096; // 4k bytes
-const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE; // 14
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 typedef struct {
-	uint32_t num_rows;
-	// テーブルのページ数。100まで
+	int file_descriptor;
+	uint32_t file_length;
 	void* pages[TABLE_MAX_PAGES];
+} Pager;
+
+typedef struct {
+	uint32_t num_rows;
+	Pager* pager;
 } Table;
 
 typedef struct {
@@ -100,8 +108,11 @@ static void serialize_row(Row* source, void* destination);
 static void deserialize_row(void* source, Row* destination);
 static void* row_slot(Table* table, uint32_t row_num);
 static void print_row(Row* row);
-static Table* new_table();
-static void free_table(Table* table);
+static Table* db_open();
+static void db_close(Table* table);
+static Pager* pager_open(const char* filename);
+static void* get_page(Pager* pager, uint32_t page_num);
+static void pager_flush(Pager* pager, uint32_t page_num, uint32_t size);
 
 static InputBuffer* new_input_buffer() {
 	InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -134,8 +145,7 @@ static void print_prompt() { printf("db > "); }
 
 static MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
 	if (strcmp(input_buffer->buffer, ".exit") == 0) {
-		close_input_buffer(input_buffer);
-		free_table(table);
+		db_close(table);
 		exit(EXIT_SUCCESS);
 	} else {
 		return META_COMMAND_UNRECOGNIZED_COMMAND;
@@ -236,35 +246,166 @@ static void deserialize_row(void* source, Row* destination) {
 
 static void* row_slot(Table* table, uint32_t row_num) {
 	uint32_t page_num = row_num / ROWS_PER_PAGE;
-	void* page = table->pages[page_num];
-	if (page == NULL) {
-		page = table->pages[page_num] = malloc(PAGE_SIZE);
-	}
+	void* page = get_page(table->pager, page_num);
 	uint32_t row_offset = row_num % ROWS_PER_PAGE;
 	uint32_t byte_offset = row_offset * ROW_SIZE;
 
 	return page + byte_offset;
 }
 
-static Table* new_table() {
+static Table* db_open(const char* filename) {
+	Pager* pager = pager_open(filename);
+	uint32_t num_rows = pager->file_length / ROW_SIZE;
+	printf("file_length: %d\n", pager->file_length);
+	printf("row_size: %d\n", ROW_SIZE); // i think 291
+
 	Table* table = (Table*)malloc(sizeof(Table));
-	table->num_rows = 0;
-	for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-		table->pages[i] = NULL;
-	}
+	table->pager = pager;
+	table->num_rows = num_rows;
 
 	return table;
 }
 
-static void free_table(Table* table) {
-	for (int i = 0; table->pages[i]; i++) {
-		free(table->pages[i]);
+// ページキャッシュをディスクにフラッシュ
+// データベースファイルを閉じる
+// ページャとテーブルのデータ構造のためのメモリを解放
+//
+static void db_close(Table* table) {
+	Pager* pager = table->pager;
+	// 全体のページ数
+	uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+	// ページキャッシュをディスクにフラッシュ
+	for (uint32_t i = 0; i < num_full_pages; i++) {
+		if (pager->pages[i] == NULL) {
+			continue;
+		}
+		pager_flush(pager, i, PAGE_SIZE);
+		free(pager->pages[i]);
+		pager->pages[i] = NULL;
 	}
+
+	// ファイルの末尾に書き込むページが部分的に存在する場合があるのでそれらもフラッシュする
+	uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+	if (num_additional_rows > 0) {
+		uint32_t page_num = num_full_pages;
+		if (pager->pages[page_num] != NULL) {
+			pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+			free(pager->pages[page_num]);
+			pager->pages[page_num] = NULL;
+		}
+	}
+
+	// データベースファイルを閉じる
+	int result = close(pager->file_descriptor);
+	if (result == -1) {
+		printf("Error closing db file.\n");
+		exit(EXIT_FAILURE);
+	}
+	for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+		void* page = pager->pages[i];
+		if (page) {
+			free(page);
+			pager->pages[i] = NULL;
+		}
+	}
+	free(pager);
 	free(table);
 }
 
+// データベースのサイズを保存し、キャッシュを削除する
+//
+static Pager* pager_open(const char* filename) {
+	int fd = open(filename,
+				O_RDWR | O_CREAT,
+				S_IWUSR | S_IRUSR
+			);
+	if (fd == -1) {
+		printf("Unable to open file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// fdの終わりまでポインタを移動する
+	off_t file_length = lseek(fd, 0, SEEK_END);
+
+	Pager* pager = malloc(sizeof(Pager));
+	pager->file_descriptor = fd;
+	pager->file_length = file_length;
+
+	// pagesのキャッシュを削除
+	for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+		pager->pages[i] = NULL;
+	}
+
+	return pager;
+}
+
+static void* get_page(Pager* pager, uint32_t page_num) {
+	if (page_num > TABLE_MAX_PAGES) {
+		printf("Tried to fetch page number out of bounds. %d > %d\n",
+				page_num, TABLE_MAX_PAGES);
+		exit(EXIT_FAILURE);
+	}
+
+	// キャッシュミス対応。ページサイズを確保する。
+	if (pager->pages[page_num] == NULL) {
+		void* page = malloc(PAGE_SIZE);
+		uint32_t num_pages = pager->file_length / PAGE_SIZE;
+
+		// PAGE_SIZEに収まり切らない時に、部分的にキャッシュを保存させる必要がある
+		if (pager->file_length % PAGE_SIZE) {
+			num_pages += 1;
+		}
+
+		// ファイル記述子のポインタをページ*4096分移動して、書き込み開始位置まで移動する
+		// 移動してから、PAGE_SIZE分取得する
+		if (page_num <= num_pages) {
+			lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+			ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+			if (bytes_read == -1) {
+				printf("Error reading file: %d\n", errno);
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		pager->pages[page_num] = page;
+	}
+
+	return pager->pages[page_num];
+}
+
+// ページキャッシュをディスクにフラッシュ
+static void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
+	if (pager->pages[page_num] == NULL) {
+		printf("Tried to flush null page\n");
+		exit(EXIT_FAILURE);
+	}
+
+	off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+	
+	if (offset == -1) {
+		printf("Error seeking: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	ssize_t bytes_written = write(pager->file_descriptor,
+				pager->pages[page_num], size);
+	
+	if (bytes_written == -1) {
+		printf("Error writing: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+}
+
 int main(int argc, char *argv[]) {
-	Table* table = new_table();
+	if (argc < 2) {
+		printf("Must supply a database filename.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	char* filename = argv[1];
+	Table* table = db_open(filename);
+
 	InputBuffer* input_buffer = new_input_buffer();
 	while(true) {
 		print_prompt();
