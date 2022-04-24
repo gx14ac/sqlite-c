9b4f76dda94f10c38e1038dd64df16eecd6e2c37
Shintarou Okumura <flekystyley@gmail.com>
2022-04-24 13:42:01 +0900

    update

--- main.c
+++ main.c
@@ -61,7 +61,9 @@ const uint32_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_
 /* Leaf Node Header Layout */
 const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
 const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
-const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+const uint32_t LEAF_NODE_NEXT_LEAF_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NEXT_LEAF_OFFSET = LEAF_NODE_NUM_CELLS_OFFSET + LEAF_NODE_NUM_CELLS_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE + LEAF_NODE_NEXT_LEAF_SIZE;
 
 /* Leaf Node Body Layout */
 const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
@@ -199,7 +201,7 @@ static void set_node_root(void* node, bool is_root);
 static void indent(uint32_t level);
 static void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level);
 static Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key);
-
+static uint32_t* leaf_node_next_leaf(void* node);
 
 static InputBuffer* new_input_buffer() {
 	InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -385,7 +387,15 @@ static void cursor_advance(Cursor* cursor) {
 
 	cursor->cell_num += 1;
 	if (cursor->cell_num >= *(leaf_node_num_cells(node))) {
-		cursor->end_of_table = true;
+		/* Advance to next leaf node */
+		uint32_t next_page_num = *leaf_node_next_leaf(node);
+    	if (next_page_num == 0) {
+ 			/* This was rightmost leaf */
+ 	     	cursor->end_of_table = true;
+ 	   	} else {
+ 	    	cursor->page_num = next_page_num;
+ 	    	cursor->cell_num = 0;
+ 	   }
 	}
 }
 
@@ -518,13 +528,10 @@ static void pager_flush(Pager* pager, uint32_t page_num) {
 }
 
 static Cursor* table_start(Table* table) {
-	Cursor* cursor = malloc(sizeof(Cursor));
-	cursor->table = table;
-	cursor->page_num = table->root_page_num;
-	cursor->cell_num = 0;
+	Cursor* cursor = table_find(table, 0);
 
-	void* root_node = get_page(table->pager, table->root_page_num);
-	uint32_t num_cells = *leaf_node_num_cells(root_node);
+	void* node = get_page(table->pager, cursor->page_num);
+	uint32_t num_cells = *leaf_node_num_cells(node);
 	cursor->end_of_table = (num_cells == 0);
 
 	return cursor;
@@ -557,7 +564,8 @@ static void* leaf_node_value(void* node, uint32_t cell_num) {
 static void initialize_leaf_node(void* node) {
 	set_node_type(node, NODE_LEAF);
 	set_node_root(node, false);
-	*(leaf_node_num_cells(node)) = 0;
+	*leaf_node_num_cells(node) = 0;
+	*leaf_node_next_leaf(node) = 0;
 }
 
 static void initialize_internal_node(void* node) {
@@ -663,6 +671,9 @@ static void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value)
 	uint32_t new_page_num = get_unused_page_num(cursor->table->pager);
 	void* new_node = get_page(cursor->table->pager, new_page_num);
 	initialize_leaf_node(new_node);
+	// リーフノードを分割するたびに、兄弟ポインターを更新
+	*leaf_node_next_leaf(new_node) = *leaf_node_next_leaf(old_node);
+	*leaf_node_next_leaf(old_node) = new_page_num;
 	
 	// すべての既存キーと新しいキーを旧ノード（左）と新ノード（右）に均等に分割
 	// 旧ノード（左）と新ノード（右）の間で均等に分割する必要
@@ -678,7 +689,8 @@ static void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value)
 	  void* destination = leaf_node_cell(destination_node, index_within_node);
 	
 	  if (i == cursor->cell_num) {
-	    serialize_row(value, destination);
+		serialize_row(value, leaf_node_value(destination_node, index_within_node));
+		*leaf_node_key(destination_node, index_within_node) = key;
 	  } else if (i > cursor->cell_num) {
 	    memcpy(destination, leaf_node_cell(old_node, i - 1), LEAF_NODE_CELL_SIZE);
 	  } else {
@@ -846,6 +858,10 @@ static Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key)
 		case NODE_INTERNAL:
 			return internal_node_find(table, child_num, key);
 	}
+}
+
+static uint32_t* leaf_node_next_leaf(void* node) {
+	return node + LEAF_NODE_NEXT_LEAF_OFFSET;
 }
 
 int main(int argc, char *argv[]) {
